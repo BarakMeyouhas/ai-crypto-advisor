86a0d2fe4ca0c7aa31b06d1a39a0c7cf2a46e5f0
Barakm25 <barakm@mountapps.io>
2026-08-15 09:51:53 +0300

    After successful Render deployment, got the apiUrl for production env, and set it in the environment.production.ts file

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.your-production-url.com/api' // Will be swapped during production build
+  apiUrl: 'https://ai-crypto-advisor-q7by.onrender.com/api' 
 };
